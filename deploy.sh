9775bb40f0334456829f0c718daf00efcaa57453
Timothy Miles-Board <drtjmb@gmail.com>
2016-07-15 10:33:02 +0100

    Add deploy script which will push to gh-pages branch

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+push () {
+    git add .
+    git commit -m "Updated pages"
+    git push
+}
+
+# http://stackoverflow.com/questions/3231804/in-bash-how-to-add-are-you-sure-y-n-to-any-command-or-alias
+confirm () {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case $response in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+cd build
+git st
+confirm "OK to push? [y/N]" && push
+cd ..
+
